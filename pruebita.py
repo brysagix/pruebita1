eabbbb82c42a2e5e59bc0b1bb69705fb22a6a14a
AlejandraVillada <61390038+AlejandraVillada@users.noreply.github.com>
2022-02-17 20:35:28 -0500

    prueba_alejandra

--- pruebita.py
+++ pruebita.py
@@ -1,19 +1,8 @@
 
 
-print("Hola soy una pruebita")
-print("Hola soy una pruebita")
-print("Hola soy una pruebita")
-print("Hola soy una pruebita")
-print("Hola soy una pruebita")
-print("Hola soy una pruebita")
-print("Hola soy una pruebita")
 
 
-
-
-
-
-print("Hola soy una pruebita")
+print("Prueba Alejandra")
 print("Hola soy una pruebita")
 print("Hola soy una pruebita")
 print("Hola soy una pruebita")
